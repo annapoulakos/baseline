300f3873fa4d35ad913a4c13992473dbf0c2aa82
Jim Poulakos <admin@jimpoulakos.com>
2015-08-26 11:50:11 -0500

    Chore(gulp): Updates gulp tasks to include sourcemaps
    Additionally, updates packages to include latest package requirements.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,45 @@
 var gulp = require('gulp'),
-    fs = require('fs'),
-    babelify = require("babelify"),
     browserify = require('browserify'),
+    babelify = require("babelify"),
     partialify = require('partialify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rename = require('gulp-rename'),
     karma = require('karma').server,
     sourcePath = 'source/**/*.js',
-    distPath = 'build/';
+    distPath = 'build/'
+    ;
 
-gulp.task('browserify', function () {
-    return browserify()
-    .transform(babelify)
-    .transform(partialify)
-    .require('./source/index.js', {
-        entry: true
-    })
-    .bundle()
-    .on('error', function (err) {
-        console.error(err.toString());
-        this.emit('end');
-    })
-    .pipe(fs.createWriteStream(distPath + '/index.js'));
+var tasks = [
+    {
+        name: 'browserify',
+        entries: './source/index.js',
+        transforms: [babelify, partialify],
+        source: 'index.js',
+        outputName: 'index.js',
+        sourceMapsDirectory: distPath,
+        outputDirectory: distPath
+    }
+];
+
+tsks.forEach(function (task) {
+    gulp.task(task.name, function () {
+        return browserify({
+            entries: task.entries,
+            transform: task.transforms
+        }).bundle()
+        .pipe(source(task.source))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .on('error', function(err) {
+            console.error(err.toString());
+            this.emit('end');
+        })
+        .pipe(rename(task.outputName))
+        .pipe(sourcemaps.write(task.sourceMapsDirectory))
+        .pipe(gulp.dest(task.outputDirectory));
+    });
 });
 
 gulp.task('karma', function (done) {
